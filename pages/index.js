a83fe74b7e324700e1ea119ec38db140be7cce94
aadesh shinde <ashinde@paktolus.com>
2024-07-16 14:16:10 +0530

    project setup

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,9 @@
-import Head from "next/head";
-import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../helpers/api-utils";
-
-function HomePage(props) {
-  const featuredEvents = props.featuredEvents;
-
+function HomePage() {
   return (
     <div>
-      <Head>
-        <title>All Events</title>
-        <meta
-          name="Description"
-          content="find lot of events that allow you to evelove..."
-        />
-      </Head>
-      <EventList items={featuredEvents} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const getAllEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      featuredEvents: getAllEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
