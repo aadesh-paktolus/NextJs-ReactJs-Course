47f80f8bbc3b94f1541e7d5f679bc135027a87f2
aadesh shinde <ashinde@paktolus.com>
2024-07-17 20:59:51 +0530

    add meetups and fetch using mongodb

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://aadesh:aofsKugK8l8OwS0K@cluster0.osgjdo6.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
